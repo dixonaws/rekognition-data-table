cd60f421848dc862398cdb712426944452416896
dixonaws@amazon.com <dixonaws@amazon.com>
2018-08-07 23:57:25 -0400

    added simpe json parsing

---
+++ README.md
@@ -0,0 +1,2 @@
+### rekognition-data-table
+simple example of writing and reading from a DynamoDB table
--- put-sample-data.py
+++ put-sample-data.py
@@ -14,7 +14,7 @@ def main():
 	dictRow = {}
 	dictRow['FaceId'] = "f35dc12b-8507-4412-a3d7-99455a3f710c"
 	dictRow[
-		'data'] = "{'name': 'John Dough', 'email': 'john@dough.com', 'mobile': '8888675309', 'cmdc_resident': 'false'}"
+		'data'] = '{"name": "John Dough", "email": "john@dough.com", "mobile": "8888675309", "cmdc_resident": "false", "reference_photo": "https://s3.amazonaws.com/doorman-dixonaws/references/john_dough.jpg"}'
 
 	response = table.put_item(Item=dictRow)
 
@@ -28,11 +28,13 @@ def main():
 
 	response = table.get_item(Key={'FaceId': 'f35dc12b-8507-4412-a3d7-99455a3f710c'})
 
-	print(json.dumps(response['Item']['data'], indent=4, separators=(',', ': ')))
+	# print(json.dumps(response['Item']['data'], indent=4, separators=(',', ': ')))
 
 
-# this doesnt work because property name are expected in double quotes
-# dictItem=json.loads(response['Item']['data'])
+	print("Loading into a Python dict...")
+	dictItem=json.loads(response['Item']['data'])
+
+	print('dictItem: ' + json.dumps(dictItem, indent=4, separators=(',', ': ')))
 
 
 main()
